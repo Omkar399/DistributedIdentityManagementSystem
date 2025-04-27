e47aee7a25d07a58d816ec17b87a64677dc29759
Omkar399 <omkarpodey@gmail.com>
2025-04-26 21:40:40 -0700

    cors

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // Frontend configuration file
 // Change this value to your AWS public URL (include the protocol http/https)
-export const API_BASE_URL = 'http://13.218.75.79:8090';
+export const API_BASE_URL = 'https://corsproxy.io/?http://13.218.75.79:8090';
 
 // Region for API requests (part of the middleware logic)
 export const API_REGION = 'asia';
