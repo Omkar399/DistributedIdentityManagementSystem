78214a7e6065c23379fcc2816df95ad389bafdc6
Omkar399 <omkarpodey@gmail.com>
2025-04-26 20:08:07 -0700

    update config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,13 @@
 const path = require('path');
 
 module.exports = {
+  // Add the devServer configuration here
+  devServer: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 8080      // Specify the port (optional if 8080 is the default)
+  },
+
+  // Keep your existing webpack configuration
   configureWebpack: {
     resolve: {
       alias: {
