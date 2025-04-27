705b48a4ff4c8bc6226633b5cb94d54ce9889817
Omkar399 <omkarpodey@gmail.com>
2025-04-27 01:16:11 -0700

    change IP

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,8 +3,11 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 
 // Backend URLs
 export const API_BASE_URL = isDevelopment 
-  ? 'http://13.218.75.79:8090'  // Direct connection in development 
+  ? 'http://34.44.164.154:8090'  // Direct connection in development 
   : '/api/proxy';               // Proxy in production
+
+//   export const API_BASE_URL = 'http://localhost:8090'  // Direct connection in development 
+
 
 export const API_REGION = 'asia';
 
